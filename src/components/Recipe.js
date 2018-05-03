e6b45f3789ee21c922f10f0671d83880bb1f79f8
MelvinTham <melvintham1997@gmail.com>
2018-05-02 20:36:57 -0400

    Created table for recipes

---
+++ src/components/Recipe.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+import * as firebase from 'firebase' 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+import {Table,TableBody,TableHeader,TableRow,TableRowColumn,TableHeaderColumn} from 'material-ui/Table';
+import index from 'material-ui/FlatButton';
+import * as AOS from 'aos';
+
+class Recipe extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            Data:'',
+        };
+    
+
+        var config ={
+            headers: {'X-Mashape-Key': 'INSERT KEY'},
+        }
+            axios.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/" + this.props.id + "/information", config
+            ).then((response) => {
+              console.log(response.data);
+              console.log(response.data.id);
+              this.setState({
+                  Data: response.data,   
+              });
+              console.log(this.state.Data);
+              }).catch(function (error) {
+                console.log(error);
+              });
+        
+  }
+
+    
+
+    
+  render() {
+       var styles = {
+            maxWidth:345,
+            margin:10
+        };  
+
+    return (    
+        <div >
+            <Card style = {styles}>
+                <CardMedia>
+                    <CardTitle>{this.state.Data.title}</CardTitle>
+                    <img src = {this.state.Data.image}/>
+                </CardMedia>
+                <CardText>
+                {this.state.Data.spoonacularSourceUrl} 
+                </CardText>
+            </Card>
+        </div>
+      );
+    
+    
+      
+    
+  }
+}
+
+
+export default Recipe;
