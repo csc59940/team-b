acf3e9e7a21633f72c53f23dc22fe0688bed7479
Samantha <sbmercado0@gmail.com>
2018-05-09 18:27:11 -0400

    group acct

--- src/base.js
+++ src/base.js
@@ -1,18 +1,17 @@
 
-
 import Rebase from 're-base';
 import firebase from 'firebase';
 
 
- var config = {
-    apiKey: "AIzaSyDyZNYlbHCVRWQvjhhphSUNtDdpviHhwD4",
-    authDomain: "food-for-thought-project.firebaseapp.com",
-    databaseURL: "https://food-for-thought-project.firebaseio.com",
-    projectId: "food-for-thought-project",
-    storageBucket: "food-for-thought-project.appspot.com",
-    messagingSenderId: "1048244421297"
-  };
-  firebase.initializeApp(config);
+var config = {
+    apiKey: "AIzaSyCnNq6ZJIy_czx-0d7viiPEZAiUA4K2XBk",
+    authDomain: "food-for-thought-59940.firebaseapp.com",
+    databaseURL: "https://food-for-thought-59940.firebaseio.com",
+    projectId: "food-for-thought-59940",
+    storageBucket: "",
+    messagingSenderId: "130909874537"
+};
+firebase.initializeApp(config);
 
 
 const firebaseApp = firebase.initializeApp(config);
