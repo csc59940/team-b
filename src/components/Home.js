41aa212e6c5d3202686e16d61da7a834048e4599
Elie Elisee Paul <marcoht023@hotmail.com>
2018-05-09 16:46:43 -0400

    Home and main page design

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,38 @@ import '../css/Home.css';
 
 
 class Home extends Component {
+
+	constructor(props){
+		super(props);
+		this.state = {
+			fillIngredients: true,
+            ingredients: '',
+            number: 5,
+            ranking: 1, 
+            Data: [],
+            foodID:939682,
+            includeNutrition:'',
+            recipeData:[],
+		}
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(e){
+	    const target = e.target;
+	    const name = target.name;
+	    const value = target.value;
+	    this.setState({
+	      [name]: value
+	    });
+	}
+
+	handleSubmit(e) {
+		e.preventDefault();
+		this.props.addIngredients(this.state);
+	}
+
 	render() {
 		return (
 			<div className= "mh-100 homepage d-flex flex-column">
@@ -16,9 +48,9 @@ class Home extends Component {
 				</div>
 				<div className="col-md-6 justify-content-end" id="userIngredient">
 					<form className="align-items-end">
-						<textarea className="form-control w-75 mt-5"  rows="10" type="text" placeholder="Enter ingredients..."></textarea>
+						<textarea className="form-control w-75 mt-5"  rows="10" type="text" name="ingredients" onChange={this.handleChange} placeholder="Enter ingredients..."></textarea>
 						<Link to={"/list"} className="text-white"> <button type="submit" id="previousList" className="btn btn-dark btn-lg align-items-end mt-3 ml-5" role="button">  Previous List  </button> </Link>
-						<Link to={"/food"} className="text-white"> <button type="submit" id="findRecipes" className="btn btn-success btn-lg align-items-end mt-3 ml-5" role="button">  Find recipes  </button> </Link>
+						<Link to={"/food"} className="text-white"> <button type="submit" id="findRecipes" className="btn btn-success btn-lg align-items-end mt-3 ml-5" onClick={this.handleSubmit} role="button">  Find recipes  </button> </Link>
 					</form>
 				</div>
 				</div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,13 +3,74 @@ import { Switch, Route } from 'react-router-dom';
 import Food from './Food';
 import Home from './Home';
 import List from './List';
+import axios from 'axios'
+import * as firebase from 'firebase' 
 
 
 class Main extends Component {
+
+	constructor(props) {
+        super(props);
+        this.state = {
+           listOfIngredients: [],
+            
+        };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.addIngredients = this.addIngredients.bind(this);
+  }
+    
+	handleChange(e){
+	    const target = e.target;
+	    const name = target.name;
+	    const value = target.value;
+	    this.setState({
+	      [name]: value
+	    });
+	}
+	    
+
+
+	handleSubmit(e){
+	    e.preventDefault();
+	    
+	    var config ={
+	    headers: {'X-Mashape-Key': 'sb7yRE1EkumshkKQaBfg6WrQFbsxp1U1JpCjsnuXQjzsuDMDaH'},
+	    params: {
+	        fillIngredients: this.state.fillIngredients,
+	        ingredients: this.state.ingredients,
+	        number: this.state.number,
+	        ranking: this.state.ranking
+	      }
+	}
+	    
+	    axios.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients", config
+	    ).then((response) => {
+	      console.log(response.data);
+	      console.log(response.data.id);
+	      this.setState({
+	          Data: response.data,
+	    
+	      });
+	        
+	      }).catch(function (error) {
+	        console.log(error);
+	      });
+	    
+	        
+	    
+	    
+	  }
+
+	  addIngredients(ingredient) {
+	  	const listOfIngredients = this.state.listOfIngredients.concat(ingredient);
+	  	this.setState({listOfIngredients: listOfIngredients});
+	  }
+
 	render() {
 		return (
 			<Switch>
-				<Route exact path='/' render={ ()=> <Home />} />
+				<Route exact path='/' render={ ()=> <Home addIngredients= {this.addIngredients} />} />
 				<Route exact path='/food' render={ ()=> <Food />} />
 				<Route exact path='/list' render={ ()=> <List />} />
 			</Switch>
