9f0cbcda424e28a876311d81cc5619560f9a2b03
Samantha <sbmercado0@gmail.com>
2018-05-16 16:50:09 -0400

    List

--- src/components/List.js
+++ src/components/List.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-
+import * as firebase from 'firebase';
 
 
 class List extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			prev:[]
+		}
+
+	}
+	
+componentDidMount(){
+	const itemsRef = firebase.database().ref('Ingredients');
+	itemsRef.on('child_added',(snapshot)=>{
+		console.log(snapshot.val());
+		let items = snapshot.val();
+		let newState = [];
+		for(let item in items){
+			newState.push({
+				ingredients:item.ingredients	
+			})
+		}
+		this.setState({
+			prev:newState
+			
+		});
+		console.log(this.state.prev);
+	
+	});
+}
+
 	render() {
 		return (
-			<p> List </p>
+			<ul>
+				{this.state.prev.map((item,index)=>{
+					return (
+						<li key={index}>{item.ingredients}</li>
+
+					)
+				})}
+			</ul>
 		)
 	}
 }
