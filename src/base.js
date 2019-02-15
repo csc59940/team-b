48f4219ac78aaf0b9888740eb176718482279472
Elie Elisee Paul <marcoht023@hotmail.com>
2019-02-15 16:52:21 -0500

    Updating Food for thought for deployment

--- src/base.js
+++ src/base.js
@@ -1,16 +1,9 @@
 
 import Rebase from 're-base';
 import firebase from 'firebase';
+import firebaseInfo from '/config/firebaseInfo'
 
-var config = {
-    apiKey: "AIzaSyCnNq6ZJIy_czx-0d7viiPEZAiUA4K2XBk",
-    authDomain: "food-for-thought-59940.firebaseapp.com",
-    databaseURL: "https://food-for-thought-59940.firebaseio.com",
-    projectId: "food-for-thought-59940",
-    storageBucket: "food-for-thought-59940.appspot.com",
-    messagingSenderId: "130909874537"
-  };
-
+var config = firebaseInfo.config;
 const firebaseApp = firebase.initializeApp(config);
 const base = Rebase.createClass(firebaseApp.database());
 
